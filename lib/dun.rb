a33b06f19740b6eab2514106663222838b618774
kayakjiang <kayak.jiang@gmail.com>
2013-05-19 10:56:12 +0800

    first commit

---
+++ lib/dun.rb
@@ -0,0 +1,56 @@
+module Dun
+  class Activity
+    attr_reader :data
+
+    def self.inherited(subclass)
+      return if Kernel.method_defined? subclass.name
+      
+      Kernel.class_eval %Q{
+        def #{subclass.name}(data = {}, &p)
+          #{subclass.name}.new(data).call(&p)
+        end
+      }
+
+    rescue SyntaxError, TypeError
+      
+    end
+    
+    def self.<<(data = {})
+      new(data).call
+    end
+
+    def self.data_reader *attrs
+      
+      attrs.each do |attr|
+        define_method attr do
+          instance_variable_get("@#{attr}") || \
+          instance_variable_set("@#{attr}", data[attr.to_sym])
+        end
+      end
+      
+    end
+    
+    def self.set name, value
+      define_method(name) { value }
+    end
+
+    def initialize(data)
+      @data = data
+    end
+    
+    def call
+      'dun'
+    end
+
+    private
+
+    def get_or_set(name)
+      value = instance_variable_get("@#{name}")
+      return value if value
+      value = yield
+      instance_variable_set("@#{name}", value)
+    end
+
+  end
+
+end
---
+++ test/activity_test.rb
@@ -0,0 +1,128 @@
+require 'test_helper'
+
+class A < Dun::Activity
+
+  def call
+    true
+  end
+  
+end
+
+module B
+  class A < Dun::Activity
+    def call
+      true
+    end
+  end
+end
+
+module B
+  class H < Dun::Activity
+    def call
+      true
+    end
+  end
+end
+
+class C
+  class A < Dun::Activity
+    def call
+      true
+    end
+  end
+end
+
+class C
+  class D < Dun::Activity
+    def call
+      true
+    end
+
+    class E < D
+      def call
+        true
+      end
+    end
+  end
+end
+
+class F < C::A
+  def call
+    true
+  end
+end
+
+class JustReturnActivitySelf < Dun::Activity
+  data_reader :a, :b, :c, :d
+
+  set :name, 'name'
+
+  def call
+    self
+  end
+
+  def foo
+    get_or_set :foo do
+      'foo'
+    end
+  end
+  
+end
+
+class ActivityTest < Test::Unit::TestCase
+
+  def test_capital_method
+    assert A()
+    assert F()
+    assert B::A()
+    assert C::D()
+    assert C::A()
+    assert C::D()
+
+    assert A     a: 'a', b: 'b'
+    assert B::A  a: 'a', b: 'b'
+    assert C::D  a: 'a', b: 'b'
+    assert C::A  a: 'a', b: 'b'
+    assert C::D  a: 'a', b: 'b'
+
+    assert A << {}
+    assert B::A << {} 
+    assert B::H << {}
+    assert C::A << {}
+    assert C::D << {}
+    assert C::D::E << {}
+    assert C::D::E << {a: 'a', b: 'b'}
+    
+  end
+
+  def test_class
+    assert A.new({}).is_a? A
+    assert B::A.new({}).is_a? B::A
+    assert C::D.new({}).is_a? C::D
+    assert C::A.new({}).is_a? C::A
+    assert C::D.new({}).is_a? C::D
+  end
+
+  def test_data_reader
+    activity = JustReturnActivitySelf(a: 'a', b: 'b', c: 'c', d: 'd')
+    
+    assert_equal activity.a, 'a'
+    assert_equal activity.b, 'b'
+    assert_equal activity.c, 'c'
+    assert_equal activity.d, 'd'
+  end
+
+  def test_set
+    activity = JustReturnActivitySelf(a: 'a')
+
+    assert_equal activity.name, 'name'
+  end
+
+  def test_get_or_set
+    activity = JustReturnActivitySelf()
+
+    assert_equal activity.foo, 'foo'
+    assert_equal activity.instance_variable_get(:@foo), 'foo'
+  end
+
+end
